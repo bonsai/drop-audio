2f6020f2cbd9ecd9bae264229d7b3ffb29f53f48
bonsai <onsen.bonsai@gmail.com>
2026-05-26 23:07:39 +0900

    Remove test.ps1

--- test.ps1
+++
@@ -1,8 +0,0 @@
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-try {
-  $r = Invoke-WebRequest -Uri 'https://drop-audio.vercel.app/mp3' -UseBasicParsing -TimeoutSec 20
-  Write-Host $r.StatusCode
-  Write-Host $r.Content
-} catch {
-  Write-Host $_.Exception.Message
-}
